e34e0e8d03e54cc476c8ce264d352acdc739cccc
Collin Han <64632875+collinaryart@users.noreply.github.com>
2024-11-15 22:25:55 +1100

    Implemented draw_pile in game.py

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 from data_structures.referential_array import ArrayR
-from player import Player
-from card import CardColor, CardLabel, Card
-from random_gen import RandomGen
+from data_structures.stack_adt import ArrayStack
 from constants import Constants
-
+from card import CardColor, CardLabel, Card
+from player import Player
+from random_gen import RandomGen
 
 class Game:
     """
@@ -13,64 +13,66 @@ class Game:
         """
         Constructor for the Game class
 
-        Args:
-            None
-
-        Returns:
-            None
-
         Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
+            Best Case Complexity: O(1)
+            Worst Case Complexity: O(1)
         """
-        raise NotImplementedError
+        self.players: ArrayR[Player] = None
+        self.draw_pile: ArrayStack[Card] = ArrayStack(Constants.DECK_SIZE)
+        self.discard_pile: ArrayStack[Card] = ArrayStack(Constants.DECK_SIZE)
+        self.current_player: Player = None
+        self.current_color: CardColor = None
+        self.current_label: CardLabel = None
 
     def generate_cards(self) -> ArrayR[Card]:
         """
         Method to generate the cards for the game
 
-        Args:
-            None
-
         Returns:
             ArrayR[Card]: The array of Card objects generated
 
         Complexity:
-            Best Case Complexity: O(N) - Where N is the number of cards in the deck
-            Worst Case Complexity: O(N) - Where N is the number of cards in the deck
+            O(N) where N is Constants.DECK_SIZE
         """
         list_of_cards: ArrayR[Card] = ArrayR(Constants.DECK_SIZE)
-        idx: int = 0
+        idx = 0
 
+        # For each color except CRAZY
         for color in CardColor:
             if color != CardColor.CRAZY:
-                # Generate 4 sets of cards from 0 to 9 for each color
-                for i in range(10):
-                    list_of_cards[idx] = Card(color, CardLabel(i))
-                    idx += 1
-                    list_of_cards[idx] = Card(color, CardLabel(i))
-                    idx += 1
+                # Add one ZERO card
+                list_of_cards[idx] = Card(color, CardLabel.ZERO)
+                idx += 1
 
-                # Generate 2 of each special card for each color
-                for i in range(2):
+                # Add two of each from ONE to NINE
+                for num in range(1, 10):  # Numbers from 1 to 9
+                    for _ in range(2):
+                        list_of_cards[idx] = Card(color, CardLabel(num))
+                        idx += 1
+
+                # Add two of each special card: SKIP, REVERSE, DRAW_TWO
+                for _ in range(2):
                     list_of_cards[idx] = Card(color, CardLabel.SKIP)
                     idx += 1
                     list_of_cards[idx] = Card(color, CardLabel.REVERSE)
                     idx += 1
                     list_of_cards[idx] = Card(color, CardLabel.DRAW_TWO)
                     idx += 1
-            else:
-                # Generate the crazy and crazy draw 4 cards
-                for i in range(4):
-                    list_of_cards[idx] = Card(CardColor.CRAZY, CardLabel.CRAZY)
-                    idx += 1
-                    list_of_cards[idx] = Card(CardColor.CRAZY, CardLabel.DRAW_FOUR)
-                    idx += 1
+        # Add CRAZY cards
+        for _ in range(4):
+            list_of_cards[idx] = Card(CardColor.CRAZY, CardLabel.CRAZY)
+            idx += 1
+            list_of_cards[idx] = Card(CardColor.CRAZY, CardLabel.DRAW_FOUR)
+            idx += 1
 
-                # Randomly shuffle the cards
-                RandomGen.random_shuffle(list_of_cards)
+        # Verify the deck size
+        if idx != Constants.DECK_SIZE:
+            raise ValueError(f"Generated {idx} cards, expected {Constants.DECK_SIZE}")
 
-                return list_of_cards
+        # Shuffle the cards
+        RandomGen.random_shuffle(list_of_cards)
+
+        return list_of_cards
 
     def initialise_game(self, players: ArrayR[Player]) -> None:
         """
@@ -79,124 +81,48 @@ class Game:
         Args:
             players (ArrayR[Player]): The array of players
 
-        Returns:
-            None
-
         Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
+            Best Case Complexity: O(N)
+            Worst Case Complexity: O(N)
+            Where N is the total number of cards
         """
-        raise NotImplementedError
+        self.players = players
 
-    def crazy_play(self, card: Card) -> None:
-        """
-        Method to play a crazy card
+        # Generate the cards
+        deck = self.generate_cards()
 
-        Args:
-            card (Card): The card to be played
+        num_players = len(players)
+        num_cards_per_player = Constants.NUM_CARDS_AT_INIT
+        total_cards = Constants.DECK_SIZE
 
-        Returns:
-            None
+        # Deal cards to players
+        # Starting from card index 0 and player index 0
+        card_index = 0
 
-        Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
-        """
-        raise NotImplementedError
+        for _ in range(num_cards_per_player):
+            for player_index in range(num_players):
+                # Give deck[card_index] to players[player_index]
+                players[player_index].add_card(deck[card_index])
+                card_index += 1
 
-    def play_reverse(self) -> None:
-        """
-        Method to play a reverse card
+        # From where we left off, add remaining cards to draw_pile
+        # Ensuring the last index of the generated cards ends up at the top of the draw_pile
+        for idx in range(card_index, total_cards):
+            self.draw_pile.push(deck[idx])
 
-        Args:
-            None
-
-        Returns:
-            None
-
-        Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
-        """
-        raise NotImplementedError
-
-    def play_skip(self) -> None:
-        """
-        Method to play a skip card
-
-        Args:
-            None
-
-        Returns:
-            None
-
-        Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
-        """
-        raise NotImplementedError
-
-    def draw_card(self, player: Player, playing: bool) -> Card | None:
-        """
-        Method to draw a card from the deck
-
-        Args:
-            player (Player): The player who is drawing the card
-            playing (bool): A boolean indicating if the player is able to play the card
-
-        Returns:
-            Card - When drawing a playable card, other return None
-
-        Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
-        """
-        raise NotImplementedError
-
-    def next_player(self) -> Player:
-        """
-        Method to get the next player
-
-        Args:
-            None
-
-        Returns:
-            Player: The next player
-
-        Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
-        """
-        raise NotImplementedError
-
-    def play_game(self) -> Player:
-        """
-        Method to play the game
-
-        Args:
-            None
-
-        Returns:
-            Player: The winner of the game
-
-        Complexity:
-            Best Case Complexity:
-            Worst Case Complexity:
-        """
-        raise NotImplementedError
-
-
-def test_case():
-    players: ArrayR[Player] = ArrayR(4)
-    players[0] = Player("Alice", 0)
-    players[1] = Player("Bob", 1)
-    players[2] = Player("Charlie", 2)
-    players[3] = Player("David", 3)
-    g: Game = Game()
-    g.initialise_game(players)
-    winner: Player = g.play_game()
-    print(f"Winner is {winner.name}")
-
-
-if __name__ == '__main__':
-    test_case()
+        # Now draw a card to be the top of the discard_pile
+        # The card should be a number card (0-9)
+        while True:
+            if self.draw_pile.is_empty():
+                # Should not happen during initialisation
+                raise Exception("Draw pile is empty during initialisation")
+            card = self.draw_pile.pop()
+            self.discard_pile.push(card)
+            if CardLabel.ZERO.value <= card.label.value <= CardLabel.NINE.value:
+                # It's a number card
+                self.current_color = card.color
+                self.current_label = card.label
+                break
+            else:
+                # Special card, continue drawing
+                continue
